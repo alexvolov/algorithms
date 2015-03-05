fbf7655a8df14587219a06675b4a56f42ddf586e
Alexander Volov <alevolov@gmail.com>
2015-03-05 22:11:31 +0300

    Added one more test for Dijkstra's algorithm impl
    Test for adjacency list was added

--- src/test/java/com/alexvolov/ads/algorithms/graph/DijkstrasAlgorithmTest.java
+++ src/test/java/com/alexvolov/ads/algorithms/graph/DijkstrasAlgorithmTest.java
@@ -42,4 +42,28 @@ public class DijkstrasAlgorithmTest {
         assertThat(actual, is(expected));
     }
 
+    @Test
+    public void test_findShortestPathList() {
+        // data
+        int[] expected = new int[]{0, 8, 10, 12, 18, 13, 21};
+
+        // prepare
+        Graph graph = new AdjacencyList(7, GraphType.WEIGHTED_DIRECTED);
+        graph.addEdge(0, 1, 8);
+        graph.addEdge(0, 2, 10);
+        graph.addEdge(0, 3, 12);
+        graph.addEdge(1, 4, 10);
+        graph.addEdge(2, 5, 3);
+        graph.addEdge(3, 6, 12);
+        graph.addEdge(4, 3, 10);
+        graph.addEdge(4, 6, 50);
+        graph.addEdge(5, 6, 8);
+
+        // test
+        int[] actual = new DijkstrasAlgorithm().getShortestPath(graph, 0);
+
+        // verify
+        assertThat(actual, is(expected));
+    }
+
 }
